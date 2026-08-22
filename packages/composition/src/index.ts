96279ec6bcc461817e2114318a38f94a645c5cd4
pmarius <69584331+marius-patrik@users.noreply.github.com>
2026-08-22 18:23:58 +0200

    Export sidebar and profile settings contracts

--- packages/composition/src/index.ts
+++ packages/composition/src/index.ts
@@ -3,3 +3,5 @@ export * from "./resolve.js";
 export * from "./catalog.js";
 export * from "./workspace.js";
 export * from "./skins.js";
+export * from "./sidebar.js";
+export * from "./profile-settings.js";
